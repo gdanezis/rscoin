65340378fd726c241193837a2d01f6b6372537e0
George Danezis <g.danezis@ucl.ac.uk>
2015-05-13 00:18:32 +0100

    New graphs

--- exp1plot.py
+++ exp1plot.py
@@ -25,17 +25,19 @@ if __name__ == "__main__":
 
     bins = np.arange(0,2, 0.075)
     # the histogram of the data
-    n, bins, patches = plt.hist((issueT, r1T, r2T), bins, normed=1, alpha=0.75, label=["Issuing","Pay (initial)","Pay (subsequent)"])
+    # n, bins, patches = plt.hist((issueT, r1T, r2T), bins, normed=1, alpha=0.75, label=["Issuing","Pay (initial)","Pay (subsequent)"])
+    n, bins, patches = plt.hist((r1T, r2T), bins, alpha=0.75, label=["Pay (run 1)","Pay (run 2)"], color=["b","r"])
 
     [p.set_hatch("/") for p in patches[0].patches]
     [p.set_hatch("\\") for p in patches[1].patches]
-    [p.set_hatch("x") for p in patches[2].patches]
+    #[p.set_hatch("x") for p in patches[2].patches]
 
 
     plt.xlabel('Latency (sec)')
-    plt.ylabel('Probability density')
-    plt.title(r'RSCoin Issue & Pay Protocol Latency')
-    plt.axis([0, 1.5, 0, 8])
+    plt.ylabel('Number of transactions')
+    # plt.title(r'RSCoin Issue & Pay Protocol Latency')
+    print n
+    plt.axis([0, 1.5, 0, max(max(n[0]), max(n[1])) + 50])
     plt.grid(True)
 
     first_legend = plt.legend(loc=1)
