25deaf902b88652c06d14fed78015ad04c5a7de4
George Danezis <g.danezis@ucl.ac.uk>
2015-05-13 00:09:53 +0100

    New fig 3

--- exp3fig.py
+++ exp3fig.py
@@ -37,28 +37,28 @@ if __name__ == "__main__":
 
     m_issue, v_issue, m_r1, v_r1, m_r2, v_r2 = map(np.array, (m_issue, v_issue, m_r1, v_r1, m_r2, v_r2))
 
-    plt.plot(X, m_issue, label="Issuing")
-    v = v_issue / (len(cnt_issue)**0.5)
-    plt.fill_between(x=X, y1=m_issue - v, y2=m_issue+v, alpha=0.2, color="b")
+    #plt.plot(X, m_issue, label="Issuing")
+    #v = v_issue / (len(cnt_issue)**0.5)
+    #plt.fill_between(x=X, y1=m_issue - v, y2=m_issue+v, alpha=0.2, color="b")
 
-    plt.plot(X, m_r1, label="Pay (initial)", linestyle="dashed")    
+    plt.plot(X, m_r1, label="Pay (run 1)", color="b") #, linestyle="dashed")    
     v = v_r1 / (len(cnt_r1)**0.5)
-    plt.fill_between(x=X, y1=m_r1 - v, y2=m_r1+v, alpha=0.2, color="g")
+    plt.fill_between(x=X, y1=m_r1 - v, y2=m_r1+v, alpha=0.2, color="b")
 
-    plt.plot(X, m_r2, label="Pay (subsequent)", linestyle="dashdot")
+    plt.plot(X, m_r2, label="Pay (run 2)", linestyle="dashdot", color="r")
     v = v_r2 / (len(cnt_r2)**0.5)
     plt.fill_between(x=X, y1=m_r2 - v, y2=m_r2+v, alpha=0.2, color="r")
 
 
     plt.xlabel('Number of Servers')
     plt.ylabel('Transactions / sec')
-    plt.title(r'Transaction Throughput Scalability')
-    plt.axis([1, 10, 0, 1700])
+    # plt.title(r'Transaction Throughput Scalability')
+    plt.axis([1, 10, 0, 800])
     plt.grid(True)
 
     first_legend = plt.legend(loc="upper left")
 
-    plt.savefig(join(directory, "Throughput.pdf"))
+    plt.savefig("Throughput.pdf")
 
     # plt.show()
     plt.close()
